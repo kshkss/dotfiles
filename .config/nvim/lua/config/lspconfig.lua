c72257adbe1c7a4fc67875d8398b7cdd4852ae55
Akishika Issui <kshkss@users.noreply.github.com>
2023-10-22 00:03:01 +0900

    refactor(neovim): concat lsp settings

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -36,7 +36,7 @@ end
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local servers = { "clangd", "rust_analyzer", "pylsp", "fortls", "elmls" }
+local servers = { "clangd", "rust_analyzer", "pylsp", "lua_ls", "fortls", "elmls" }
 for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup {
 		on_attach = on_attach,
@@ -58,32 +58,26 @@ for _, lsp in ipairs(servers) do
 					},
 				},
 			},
+			Lua = {
+			-- depend on sumneko/lua-language-server and stylua for formatter
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { "vim" },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = {
+					enable = false,
+				},
+			},
 		},
 	}
 end
 
--- depend on sumneko/lua-language-server and stylua for formatter
-nvim_lsp.lua_ls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
-})
