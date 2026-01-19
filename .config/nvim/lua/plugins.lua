cef2b16558bd5f08780e1c1e64c4ce6914fc8097
Akishika, Issui <kshkss@users.noreply.github.com>
2026-01-19 09:29:01 +0900

    feat(nvim): change default model for CopilotChat.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -134,7 +134,7 @@ return require("lazy").setup({
 			opts = function()
 				require("config/copilot-chat")
 				return {
-					model = 'gpt-4.1', -- AI model to use
+					model = 'gpt-5', -- AI model to use
 					temperature = 0.1, -- Lower = focused, higher = creative
 					window = {
 						layout = 'vertical', -- 'vertical', 'horizontal', 'float'
